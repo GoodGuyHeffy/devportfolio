5adf5794afde2858cf520b34b678fa04d61e62c4
GoodGuyHeffy <95945343+GoodGuyHeffy@users.noreply.github.com>
2023-06-21 19:40:17 -0500

    New Portfolio, navbar, homepage are complete.

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+
+            <div>
+
+                <div>
+                    <p>About</p>
+                </div>
+                <p>My name is Josh and I'm a self-taught front-end developer.
+                   I'm a fan of anything outdoors, my dog Mako, my cat Pooh-Bear,  my African Cichlids, as well as video games.
+                    I'm enthusiastic for front-end frameworks like React and Tailwind. I'm always willing to learn more/refine what I already know,
+                     and ready to welcome more skills as they come!</p>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default About;
