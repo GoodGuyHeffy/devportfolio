812b04dfb111565666ec3c8ad93096129b011f2c
GoodGuyHeffy <95945343+GoodGuyHeffy@users.noreply.github.com>
2023-06-22 01:06:08 -0500

    new build folder

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillHeart } from 'react-icons/ai';
 function Footer() {
     return (
         <div className="bg-black text-white justify-center flex p-5">
-            <h3 className="inline-flex">Made with  <AiFillHeart /> By Josh Hoffert 2023</h3>
+            <h3 className="inline-flex">Made with  <AiFillHeart /> - Josh Hoffert 2023</h3>
         </div>
     )
 }
